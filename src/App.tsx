06ea9c30882e0a5fe5d3ee85d1035c6e05281174
miguel11065 <126350222+miguel11065@users.noreply.github.com>
2023-09-14 15:45:35 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
 import GlobalStyle, { Container } from './styles'
 
 function App() {
